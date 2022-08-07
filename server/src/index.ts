861e03d62c7b4b46ca375fdf3bdac3ecbc34711b
trickypr <trickypr@icloud.com>
2022-08-07 19:54:24 +1000

    feat(server): :sparkles: Allow the API server to serve the static app

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,16 @@
 import express from 'express'
 import * as trpcExpress from '@trpc/server/adapters/express'
 import cors from 'cors'
+import { existsSync } from 'fs'
 import winston from 'winston'
-
 
 import { trpcRouter } from './router'
 
 const app = express()
+
+// We only want to serve the UI in production. In development we will handle it
+// elsewhere
+if (existsSync('ui')) app.use(express.static('ui'))
 
 app.use(express.json())
 app.use(cors())
