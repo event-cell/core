4e171e595cb5240a903eddce23e2b821d024934f
vanteer <vanteer@icloud.com>
2022-09-17 22:38:02 +1000

    feat(client): Announcer display board

--- client/src/pages/announcer.tsx
+++ client/src/pages/announcer.tsx
@@ -14,12 +14,10 @@ import {
 } from '@mui/material'
 
 import { trpc } from '../App'
-import { CarCrash, Timer } from '@mui/icons-material'
+import { Timer } from '@mui/icons-material'
 import { ResultsTable } from '../components/table'
 import { useEffect } from 'react'
 import { requestWrapper } from '../components/requestWrapper'
-
-let displayInterval: any
 
 const SecondaryPaper = styled(Paper)(({ theme }) => ({
   padding: theme.spacing(1),
@@ -39,12 +37,14 @@ export const Announcer = () => {
   const runCount = trpc.useQuery(['runs.count'])
 
   useEffect(() => {
-    if (displayInterval) clearTimeout(displayInterval)
-    displayInterval = setTimeout(() => {
-      currentCompetitor.refetch()
-      allRuns.refetch()
-      runCount.refetch()
+    const timeout = setTimeout(async () => {
+      await Promise.all([
+        currentCompetitor.refetch(),
+        allRuns.refetch(),
+        runCount.refetch(),
+      ])
     }, 1000 * 4)
+    return () => clearTimeout(timeout)
   }, [currentCompetitor, allRuns, runCount])
 
   const requestErrors = requestWrapper(currentCompetitor, allRuns, runCount)
@@ -99,17 +99,19 @@ export const Announcer = () => {
   let split1 = 0.0
   let split2 = 0.0
   let time = 0.0
-  let split1Colour = 'background.default'
-  let split2Colour = 'background.default'
-  let timeColour = 'background.default'
-  let split1txtColour = 'background.default'
-  let split2txtColour = 'background.default'
-  let timetxtColour = 'background.default'
+  let launchColour = 'background.default'
+  let sector1Colour = 'background.default'
+  let sector2Colour = 'background.default'
+  let finishColour = 'background.default'
 
   let bestLaunch = 9999999.0
+  let previousBestLaunch = 9999999.0
   let bestSector1 = 9999999.0
+  let previousBestSector1 = 9999999.0
   let bestSector2 = 9999999.0
+  let previousBestSector2 = 9999999.0
   let bestFinishTime = 9999999.0
+  let previousBestFinishTime = 9999999.0
   let personalBestLaunch = 9999999.0
   let previousPersonalBestLaunch = 9999999.0
   let personalBestSector1 = 9999999.0
@@ -150,12 +152,19 @@ export const Announcer = () => {
       for (const run of person.times) {
         if (typeof run !== 'undefined' && run.status === 0) {
           if (run.split1 < bestLaunch) {
+            previousBestLaunch = bestLaunch
             bestLaunch = run.split1
           }
-          if (run.split2 < bestSector1) {
-            bestSector1 = run.split2
+          if (run.split2 - run.split1 < bestSector1) {
+            previousBestSector1 = bestSector1
+            bestSector1 = run.split2 - run.split1
+          }
+          if (run.time - run.split2 < bestSector2) {
+            previousBestSector2 = bestSector2
+            bestSector2 = run.time - run.split2
           }
           if (run.time < bestFinishTime) {
+            previousBestFinishTime = bestFinishTime
             bestFinishTime = run.time
           }
         }
@@ -164,64 +173,38 @@ export const Announcer = () => {
   }
 
   if (split1 <= bestLaunch && split1 > 0) {
-    split1Colour = 'purple'
-    split1txtColour = 'default'
+    launchColour = 'purple'
   } else if (split1 <= personalBestLaunch && split1 > 0) {
-    split1Colour = 'green'
-    split1txtColour = 'default'
+    launchColour = 'green'
   } else if (split1 > 0) {
-    split1Colour = 'yellow'
-    split1txtColour = 'default'
+    launchColour = 'yellow'
   }
 
-  if (split2 <= bestSector1 && split2 > 0) {
-    split2Colour = 'purple'
-    split2txtColour = 'default'
-  } else if (split2 <= personalBestSector1 && split2 > 0) {
-    split2Colour = 'green'
-    split2txtColour = 'default'
+  if (split2 - split1 <= bestSector1 && split2 > 0) {
+    sector1Colour = 'purple'
+  } else if (split2 - split1 <= personalBestSector1 && split2 > 0) {
+    sector1Colour = 'green'
   } else if (split2 > 0) {
-    split2Colour = 'yellow'
-    split2txtColour = 'default'
+    sector1Colour = 'yellow'
+  }
+
+  if (time - split2 <= bestSector2 && time > 0) {
+    sector2Colour = 'purple'
+  } else if (time - split2 <= personalBestSector2 && time > 0) {
+    sector2Colour = 'green'
+  } else if (time > 0) {
+    sector2Colour = 'yellow'
   }
 
   if (time <= bestFinishTime && time > 0) {
-    timeColour = 'purple'
-    timetxtColour = 'default'
+    finishColour = 'purple'
   } else if (time <= personalBestFinishTime && time > 0) {
-    timeColour = 'green'
-    timetxtColour = 'default'
+    finishColour = 'green'
   } else if (time > 0) {
-    timeColour = 'yellow'
-    timetxtColour = 'default'
+    finishColour = 'yellow'
   }
 
   // Render functions
-  const renderTime = () => {
-    const idx = currentRun.times.length - 1
-    const times = currentRun.times[idx]
-
-    if (typeof times !== 'undefined') {
-      if (times.status === 2) {
-        return (
-          <Grid sx={{ color: 'white' }}>
-            <CarCrash color="error" sx={{ fontSize: 200 }} />
-            DNF
-          </Grid>
-        )
-      } else if (times.status === 3) {
-        return (
-          <Grid sx={{ color: 'white' }}>
-            <CarCrash color="error" sx={{ fontSize: 200 }} />
-            DSQ
-          </Grid>
-        )
-      } else {
-        return time.toFixed(2)
-      }
-    }
-  }
-
   const renderClassList = () => {
     return currentClassList.map((eventClass) => (
       <Box key={eventClass.carClass.class} textAlign="left">
@@ -260,35 +243,73 @@ export const Announcer = () => {
     const times = currentRun.times[idx]
 
     if (typeof times !== 'undefined') {
-      let Launch
-      let Sector1
-      let Sector1Time
-      let Finish
-      let FinishTime
-      let LaunchDeltaPB
+      let launch: number = 0
+      let sector1: number = 0
+      let sector2: number = 0
+      let finish: number = 0
+      let finishTime: number = 0
+      let launchDeltaPB: number = 0
+      let launchDeltaLeader: number = 0
+      let sector1DeltaPB: number = 0
+      let sector1DeltaLeader: number = 0
+      let sector2DeltaPB: number = 0
+      let sector2DeltaLeader: number = 0
+      let finishDeltaPB: number = 0
+      let finishDeltaLeader: number = 0
 
       if (times.split1 > 0) {
-        Launch = (times.split1 / 1000).toFixed(2)
-        if ((times.split1 = personalBestLaunch)) {
-          LaunchDeltaPB = (
-            (previousPersonalBestLaunch - times.split1) /
-            1000
-          ).toFixed(2)
+        launch = times.split1
+        if (launch === personalBestLaunch) {
+          launchDeltaPB = launch - previousPersonalBestLaunch
         } else {
-          LaunchDeltaPB = ((personalBestLaunch - times.split1) / 1000).toFixed(
-            2
-          )
+          launchDeltaPB = launch - personalBestLaunch
+        }
+        if (launch === bestLaunch) {
+          launchDeltaLeader = launch - previousBestLaunch
+        } else {
+          launchDeltaLeader = launch - bestLaunch
         }
       }
 
       if (times.split2 > 0) {
-        Sector1Time = (times.split2 / 1000).toFixed(2)
-        Sector1 = (times.split2 / 1000 - times.split1 / 1000).toFixed(2)
+        sector1 = times.split2 - times.split1
+        if (sector1 === personalBestSector1) {
+          sector1DeltaPB = sector1 - previousPersonalBestSector1
+        } else {
+          sector1DeltaPB = sector1 - personalBestSector1
+        }
+        if (sector1 === bestSector1) {
+          sector1DeltaLeader = sector1 - previousBestSector1
+        } else {
+          sector1DeltaLeader = sector1 - bestSector1
+        }
       }
 
       if (times.time > 0) {
-        FinishTime = (times.time / 1000).toFixed(2)
-        Finish = (times.time / 1000 - times.split2 / 1000).toFixed(2)
+        finishTime = times.time
+        sector2 = times.time - times.split2
+
+        if (sector2 === personalBestSector2) {
+          sector2DeltaPB = sector2 - previousPersonalBestSector2
+        } else {
+          sector2DeltaPB = sector2 - personalBestSector2
+        }
+        if (sector2 === bestSector2) {
+          sector2DeltaLeader = sector2 - previousBestSector2
+        } else {
+          sector2DeltaLeader = sector2 - bestSector2
+        }
+
+        if (finishTime === personalBestFinishTime) {
+          finishDeltaPB = finishTime - previousPersonalBestFinishTime
+        } else {
+          finishDeltaPB = finishTime - personalBestFinishTime
+        }
+        if (finishTime === bestFinishTime) {
+          finishDeltaLeader = finishTime - previousBestFinishTime
+        } else {
+          finishDeltaLeader = finishTime - bestFinishTime
+        }
       }
 
       return (
@@ -311,13 +332,32 @@ export const Announcer = () => {
                     sx={{
                       width: 20,
                       height: 20,
-                      backgroundColor: split1Colour,
+                      backgroundColor: launchColour,
                       borderRadius: '4px',
                     }}
                   />
                 </TableCell>
-                <TableCell>{Launch}</TableCell>
-                <TableCell>{LaunchDeltaPB}</TableCell>
+                <TableCell>
+                  {launch !== 0
+                    ? launch > 0
+                      ? '+' + (launch / 1000).toFixed(2)
+                      : (launch / 1000).toFixed(2)
+                    : ''}
+                </TableCell>
+                <TableCell>
+                  {launchDeltaPB !== 0
+                    ? launchDeltaPB > 0
+                      ? '+' + (launchDeltaPB / 1000).toFixed(2)
+                      : (launchDeltaPB / 1000).toFixed(2)
+                    : ''}
+                </TableCell>
+                <TableCell>
+                  {launchDeltaLeader !== 0
+                    ? launchDeltaLeader > 0
+                      ? '+' + (launchDeltaLeader / 1000).toFixed(2)
+                      : (launchDeltaLeader / 1000).toFixed(2)
+                    : ''}
+                </TableCell>
               </TableRow>
               <TableRow>
                 <TableCell>Sector 1</TableCell>
@@ -326,13 +366,32 @@ export const Announcer = () => {
                     sx={{
                       width: 20,
                       height: 20,
-                      backgroundColor: split2Colour,
+                      backgroundColor: sector1Colour,
                       borderRadius: '4px',
                     }}
                   />
                 </TableCell>
-                <TableCell>{Sector1}</TableCell>
-                <TableCell></TableCell>
+                <TableCell>
+                  {sector1 !== 0
+                    ? sector1 > 0
+                      ? '+' + (sector1 / 1000).toFixed(2)
+                      : (sector1 / 1000).toFixed(2)
+                    : ''}
+                </TableCell>
+                <TableCell>
+                  {sector1DeltaPB !== 0
+                    ? sector1DeltaPB > 0
+                      ? '+' + (sector1DeltaPB / 1000).toFixed(2)
+                      : (sector1DeltaPB / 1000).toFixed(2)
+                    : ''}
+                </TableCell>
+                <TableCell>
+                  {sector1DeltaLeader !== 0
+                    ? sector1DeltaLeader > 0
+                      ? '+' + (sector1DeltaLeader / 1000).toFixed(2)
+                      : (sector1DeltaLeader / 1000).toFixed(2)
+                    : ''}
+                </TableCell>
               </TableRow>
               <TableRow>
                 <TableCell>Sector 2</TableCell>
@@ -341,26 +400,66 @@ export const Announcer = () => {
                     sx={{
                       width: 20,
                       height: 20,
-                      backgroundColor: timeColour,
+                      backgroundColor: sector2Colour,
                       borderRadius: '4px',
                     }}
                   />
-                </TableCell>{' '}
-                <TableCell>{Finish}</TableCell>
+                </TableCell>
+                <TableCell>
+                  {sector2 !== 0
+                    ? sector2 > 0
+                      ? '+' + (sector2 / 1000).toFixed(2)
+                      : (sector2 / 1000).toFixed(2)
+                    : ''}
+                </TableCell>
+                <TableCell>
+                  {sector2DeltaPB !== 0
+                    ? sector2DeltaPB > 0
+                      ? '+' + (sector2DeltaPB / 1000).toFixed(2)
+                      : (sector2DeltaPB / 1000).toFixed(2)
+                    : ''}
+                </TableCell>
+                <TableCell>
+                  {sector2DeltaLeader !== 0
+                    ? sector2DeltaLeader > 0
+                      ? '+' + (sector2DeltaLeader / 1000).toFixed(2)
+                      : (sector2DeltaLeader / 1000).toFixed(2)
+                    : ''}
+                </TableCell>
               </TableRow>
               <TableRow>
-                <TableCell>Finish</TableCell>
+                <TableCell>Finish Time</TableCell>
                 <TableCell>
                   <Box
                     sx={{
                       width: 20,
                       height: 20,
-                      backgroundColor: timeColour,
+                      backgroundColor: finishColour,
                       borderRadius: '4px',
                     }}
                   />
                 </TableCell>
-                <TableCell>{FinishTime}</TableCell>
+                <TableCell>
+                  {finishTime !== 0
+                    ? finishTime > 0
+                      ? '+' + (finishTime / 1000).toFixed(2)
+                      : (finishTime / 1000).toFixed(2)
+                    : ''}
+                </TableCell>
+                <TableCell>
+                  {finishDeltaPB !== 0
+                    ? finishDeltaPB > 0
+                      ? '+' + (finishDeltaPB / 1000).toFixed(2)
+                      : (finishDeltaPB / 1000).toFixed(2)
+                    : ''}
+                </TableCell>
+                <TableCell>
+                  {finishDeltaLeader !== 0
+                    ? finishDeltaLeader > 0
+                      ? '+' + (finishDeltaLeader / 1000).toFixed(2)
+                      : (finishDeltaLeader / 1000).toFixed(2)
+                    : ''}
+                </TableCell>
               </TableRow>
             </TableBody>
           </MUITable>
