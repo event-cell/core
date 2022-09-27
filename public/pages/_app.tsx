e36bb6f7de7fb56bf36980fdeef74070fe39e80e
trickypr <trickypr@icloud.com>
2022-09-27 20:48:14 +1000

    🐛 Fix things for deploy

--- public/pages/_app.tsx
+++ public/pages/_app.tsx
@@ -1,6 +1,6 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import { Theme } from '../shared/components/Theme'
+import { Theme } from '../src/shared/components/Theme'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
